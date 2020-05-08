c58bd571c576207bf69c92a6299622cb2bb1acb9
gabriel_bispo@me.com <gabriel_bispo@me.com>
2020-05-08 18:52:43 -0300

    Updated Project

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,30 @@
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const btOpen = document.getElementById('btn-open');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
-const btnFooter = document.querySelector('#footer button')
+const btnFooter = document.querySelector('#footer button');
 
 btnFooter.addEventListener('click', e => {
-    windows.scrollTo(0, 0)
+    window.scrollTo(0, 0);
 })
 
-btOpen.addEventListener('click', e=> {
+
+
+
+btOpen.addEventListener('click', e => {
     body.classList.add('open-menu');
 })
 
-closeMenu.forEach( el => {
+
+closeMenu.forEach(el => {
     el.addEventListener('click', e => {
         body.classList.remove('open-menu');
     })
 })
 
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 44.531274, lng: 10.868766},
+        center: { lat: 44.531274, lng: 10.868766 },
         zoom: 14
     });
 }
